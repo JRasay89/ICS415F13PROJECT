f9cc072a9481885b53337f18d1efe51a4c409f79
bryansl <bsl@hawaii.edu>
2013-11-22 14:16:43 -1000

    database and table creation
    creates a database called 'forum_project' with 7 tables 1 is users and
    the other 6 are the 6 topics we currently have.   also adds a 'guest'
    account to users table

---
+++ check.php
@@ -0,0 +1,99 @@
+<?php
+	$username = "bryan";
+	$password = "password";
+	$dbName = "forum_project";
+	//connect
+	$connection = mysqli_connect("localhost", $username, $password);
+	if(mysqli_connect_errno($connection)){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	
+	//create database
+	function createDatabase($con)
+	{
+		global $dbName;
+		$dbCreate = "CREATE DATABASE ".$dbName;
+		if(mysqli_query($con, $dbCreate))
+		{
+
+		}
+		else
+		{
+			#echo"<script>alert(\"failed to make db". mysqli_error($con) ."\")</script>";
+		}
+	}
+	
+	function connectToDB($con)
+	{
+		global $dbName;
+		mysqli_select_db($con, $dbName);
+	}
+	
+	$topics = array
+		(
+			"gaming",
+			"movies",
+			"music",
+			"hardware",
+			"comics",
+			"other"
+		);
+	//create tables if they dont exist
+	function createTables($con)
+	{
+		$userTable="CREATE TABLE users
+			(
+				userID INT NOT NULL AUTO_INCREMENT,
+				PRIMARY KEY(userID),
+				username CHAR(40) NOT NULL UNIQUE,
+				name CHAR(40) NOT NULL,
+				email CHAR(100),
+				age INT,
+				password CHAR(50) NOT NULL
+			)";
+
+
+		global $topics;
+		foreach($topics as $topic)
+		{
+			$topicTable="CREATE TABLE ".$topic."
+				(
+					topicID INT NOT NULL AUTO_INCREMENT,
+					PRIMARY KEY(topicID),
+					subject CHAR(40) NOT NULL,
+					userID INT NOT NULL
+				)";
+			mysqli_query($con, $topicTable);
+			if(mysqli_error($con))
+			{
+				echo "error: " . mysqli_error($con);
+			}
+		}
+		if(!mysqli_query($con,$userTable))
+		{
+			#echo "error: " . mysqli_error($con);
+		}
+	}
+	
+	function addGuest($con)
+	{
+		$query = "INSERT INTO users(username, name, email, age, password)
+				VALUES('guest', 'guest', 'guest@guest.com', 0, 1234)";
+		mysqli_query($con, $query);
+	}
+
+
+	
+	//adds the current default topics to the empty topics table
+	function addTopics($con)
+	{
+
+	}
+
+	createDatabase($connection);
+	connectToDB($connection);
+	createTables($connection);
+	addGuest($connection);
+	addTopics($connection);
+	mysqli_close($connection);
+?>
