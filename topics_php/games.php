287f0e0d0e66ff1cf5afc734dc482293e4d251d7
John R <newtype187@yahoo.com>
2013-11-24 00:03:29 -1000

    Added php stuff, and games_message.php
    Going to create 6 message topic page to display the messages of the
    threads.

--- topics_php/games.php
+++ topics_php/games.php
@@ -16,6 +16,24 @@
 		<script src="../js/topics.js"></script>
 	</head>
 	<body>
+		<!--Connecting to Database-->
+		<?php
+			// Create connection to mysql
+			//TABLES ARE ALSO CREATED
+			//USER TABLE uses unique names and is not case sensitive
+			$host = "localhost";
+			$user = "bryan";
+			$pwd = "password";
+			$db = "forum_project";
+			$con=mysqli_connect($host,$user,$pwd);
+			if(mysqli_connect_errno($con)){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+			//Connect to the database
+			else {
+				mysqli_select_db($con, $db);
+			}
+		?>
 		<div class="container">
 			<div class="header">
 				<ul class="nav nav-pills pull-right">
@@ -40,7 +58,7 @@
 					<button type="button" class="btn btn-xs btn-primary" id="btn_topic"><span class="glyphicon glyphicon-pencil"></span> New Topic</button>
 				</p>
 				<!--Form for creating new topics-->
-				<form role="form" id="form_topic" action="#" method="post">
+				<form role="form" id="form_topic" action="games_message.php" method="post">
 					<div class="col-md-9" id="form_title">
 							<h4>Create New Topic</h4>
 					</div>
@@ -49,7 +67,7 @@
 							<label for="inputTitle" class="control-label">Enter Title:</label>
 							<div class="row">
 								<div class="col-md-9">
-									<input type="text" class="form-control" id="inputTitle" placeholder="Subject" maxlength="10">
+									<input type="text" class="form-control" id="inputTitle" name="title" placeholder="Subject" maxlength="10">
 								</div>
 							</div>
 						</div>
@@ -59,14 +77,14 @@
 							<label for="inputMessage" class="control-label">Enter Message:</label>
 							<div class="row">
 								<div class="col-md-9">
-									<textarea class="form-control" id="inputMessage" placeholder="Message" maxlength="50"></textarea>
+									<textarea class="form-control" id="inputMessage" name="message" placeholder="Message" maxlength="50"></textarea>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div class="form-group">
 						<div class="col-md-9">
-							<button type="submit" class="btn btn-default">Submit</button>
+							<button type="submit" class="btn btn-primary">Create</button>
 							<button type="button" class="btn btn-default" id="btn_form_cancel">Cancel</button>
 						</div>
 					</div>
@@ -78,6 +96,14 @@
 				<tr>
 					<th>Subject</th>
 					<th>Created By</th>
+				</tr>
+				<tr>
+					<!--Print Data-->
+					<?php
+						//EXAMPLE
+						echo "<td> Batman Sucks</td>";
+						echo "<td> Joe111 </td>";
+					?>
 				</tr>
 			</table>
 			<div class="footer">
