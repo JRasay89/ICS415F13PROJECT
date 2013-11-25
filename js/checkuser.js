91c4a53b5ec99844afdea15ba9e9d5620da7e38f
bryansl <bsl@hawaii.edu>
2013-11-24 22:16:16 -1000

    created logout function
    you can now log out using the functions in checkuser.js
    logout button changes if the user is registered or a guest

--- js/checkuser.js
+++ js/checkuser.js
@@ -1,5 +1,7 @@
 $(document).ready(function()
 {
+	var username = "";
+
 	$.ajax(
 	{
 		type:'get',
@@ -20,8 +22,23 @@ $(document).ready(function()
 				//if it gets back something that isn't false then
 				//display the welcome message
 				$("#welcome_message").html("Welcome, " + output);
+				username = output;
 			}
 		}
 	});
+	
+	if(username == 'guest')
+	{
+		$("#logout_link").html("Sign-in/Register");
+	}
+	
 
 });
+
+function logout()
+{
+	$.post('remove_cookie.php',function()
+	{
+		window.location.href = 'menu.html';
+	});
+}
