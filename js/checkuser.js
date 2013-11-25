6ffef059f9f705af00dca6459ab0f1c520f94b4b
bryansl <bsl@hawaii.edu>
2013-11-24 19:32:41 -1000

    changed user checking
    checks the user cookie, if there is none then no one is logged in so
    send them back to home page to log in/register.
    in topics.js it checks if the current user is a guest or a registered
    user and sets the variable accordingly

--- js/checkuser.js
+++ js/checkuser.js
@@ -9,9 +9,18 @@ $(document).ready(function()
 		success:function(output)
 		{
 			//will return false or a username
-			//but if this is used on a page that isn't the home page
-			//then it should never get false
-			$("#welcome_message").html("Welcome, " + output);
+			if(output === 'false')
+			{
+				//if it is false then that means no one is logged in
+				//so send them to the home page to login/register
+				window.location.href = 'home.html';
+			}
+			else
+			{
+				//if it gets back something that isn't false then
+				//display the welcome message
+				$("#welcome_message").html("Welcome, " + output);
+			}
 		}
 	});
 
--- js/topics.js
+++ js/topics.js
@@ -1,7 +1,32 @@
 //GLOBAL VARIABLES
-var is_login = true; //Boolean variable use to check if user is login or a guess, false=guess, true=user's login
+var is_login = true; //Boolean variable use to check if user is login or a guest, false=guest, true=user's login
+
+
 
 $(document).ready(function(){
+	//needed to use a document.ready function to do
+	//this check as soon as possible so i put it here
+	$.ajax(
+	{
+		type:'get',
+		cache:false,
+		async:false,
+		url:'check.php',
+		success:function(output)
+		{
+			//will return guest or a username
+			if(output === 'guest')
+			{
+				is_login = false;
+			}
+			else
+			{
+				is_login = true;
+			}
+
+		}
+	});
+
    //If true show new topic button, else hide
   if (is_login) {
 	$("#new_topic").show();
